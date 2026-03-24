b0338d9e399a5833bbf3786810be2010f0a8dc74
Adam Lyttle <40189502+adamlyttleapps@users.noreply.github.com>
2026-03-24 11:22:12 +1100

    Initial Commit

---
+++ Botdock/BotdockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotdockApp.swift
+//  Botdock
+//
+//  Created by Adam Lyttle on 24/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct BotdockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
